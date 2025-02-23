6695c5deccc0b28710aca1e535c0d99334a2ce81
NotN465 <eceljaK@gmail.com>
2025-02-23 01:09:24 +0100

    Made some bug fixes and added a option to add and delete coins, also reworked the whole coin storing system, now the users are stored together but some data is stored on the specific servers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from datetime import datetime,timedelta
 import discord
 from discord.ext import commands
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String,func
 from sqlalchemy.orm.attributes import flag_modified
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,6 +11,7 @@ from models import User,Bank,Inventory,Stocks,Server
 import os
 import json
 import asyncio
+import math
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -20,6 +21,8 @@ engine = create_engine('sqlite:///UserInfo.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 developer_status = ["411797205020704768"]
+def floor_decimal(number,decimal_places=2):
+    return math.floor(number*10**2)/10**2
 def level_xp(user_id):
     xp = session.query(User).filter_by(id=str(user_id)).first().xp
     xp_needed = 500
@@ -269,7 +272,6 @@ async def update_dionice(server_id):
 @bot.event
 async def on_ready():
     print(f'Logged on as {bot.user}!')
-    #os.system('python "stock market.py"')
     for guild in bot.guilds:
         asyncio.create_task(update_dionice(int(guild.id)))
 @bot.command()
@@ -735,8 +737,67 @@ async def moje_dionice(ctx):
                 user_stocks.stocks[stock_key] = 0
                 flag_modified(user_stocks, "stocks")
                 session.commit()
-            dionice_embed.add_field(name=f"{stock_key}",value=f"Kolicina dionice: {user_stocks.stocks[stock_key]}",inline=False)
+            dionice_embed.add_field(name=f"{stock_key}",value=f"Kolicina dionice: {round(user_stocks.stocks[stock_key],2)}\nKolicina u kunama: {round(user_stocks.stocks[stock_key]*stock_value,2)}kn",inline=False)
         await ctx.send(embed=dionice_embed)
+    else:
+        await ctx.send("Korisnik nije pronadjen, !NapraviProfil za napraviti racun!")
+@bot.command()
+async def kupi_dionice(ctx):
+    if str(ctx.author.id) in ids():
+        message = ctx.message.content
+        message = message.split(" ")
+        if len(message) == 3:
+            user_stocks = session.query(Stocks).filter_by(user_id=int(ctx.author.id)).first()
+            user_bank = session.query(Bank).filter_by(user_id=int(ctx.author.id)).first()
+            server = session.query(Server).filter_by(server_id=ctx.guild.id).first()
+            server_stocks_data = server.stocks
+            if message[1] in server_stocks_data.keys():
+                if message[1] not in user_stocks.stocks.keys():
+                    user_stocks.stocks[message[1]] = 0
+                    flag_modified(user_stocks, "stocks")
+                    session.commit()
+                if int(message[2]) > 0 and int(message[2]) < user_bank.money:
+                    user_stocks.stocks[message[1]] += int(message[2])/server_stocks_data[message[1]]
+                    flag_modified(user_stocks, "stocks")
+                    session.commit()
+                    await ctx.send(f"Uspesno kupljeno {math.floor(int(message[2])/server_stocks_data[message[1]]*1000)/1000} dionica {message[1]}")
+                else:
+                    await ctx.send("Nemate dovoljno novca na racunu")
+            else:
+                await ctx.send(f"Ne postoji dionica sa imenom {message[1]}")
+        else:
+            await ctx.send("Pogresan format komande! Upisi !dionice_kupi <dionica> <kolicina za kupnju u kunama>")
+    else:
+        await ctx.send("Korisnik nije pronadjen, !NapraviProfil za napraviti racun!")
+@bot.command()
+async def prodaj_dionice(ctx):
+    if str(ctx.author.id) in ids():
+        message = ctx.message.content
+        message = message.split(" ")
+        if len(message) == 3:
+            user_stocks = session.query(Stocks).filter_by(user_id=int(ctx.author.id)).first()
+            user_bank = session.query(Bank).filter_by(user_id=int(ctx.author.id)).first()
+            server = session.query(Server).filter_by(server_id=ctx.guild.id).first()
+            servers_stocks = server.stocks
+            if message[1] in list(servers_stocks.keys()):
+                if message[1] in list(user_stocks.stocks.keys()):
+                    to_stock = int(message[2])/servers_stocks[message[1]]
+                    if user_stocks.stocks[message[1]] > to_stock:
+                        print(to_stock)
+                        user_stocks.stocks[message[1]] -= to_stock
+                        flag_modified(user_stocks, "stocks")
+                        session.commit()
+                        user_bank.money += int(message[2])
+                        flag_modified(user_bank, "money")
+                        session.commit()
+                        await ctx.send(f"Uspjesno ste prodali {message[1]} kolicine {floor_decimal(int(message[2])/servers_stocks[message[1]],3)} na {message[2]}kn")
+                else:
+                    await ctx.send(f"Ne mozes prodati ovu dionicu jer niste kupili nijednu!")
+        else:
+            await ctx.send("Pogresan format komande! Upisi !dionice_prodaj <dionica> <kolicina za prodaju u kunama>")
+    else:
+        await ctx.send("Korisnik nije pronadjen, !NapraviProfil za napraviti racun!")
+
 
 @bot.command()
 async def rulet(ctx):
@@ -961,7 +1022,33 @@ async def napravi_coin(ctx):
                     await ctx.send(f"Uspjesno napravljen novi coin {message[1]} sa vrijednosti {message[2]}kn po coinu!")
             else:
                 await ctx.send("Pogresan format komande! Upisi !napravi_coin <ime_coina> <vrijednost u kn>")
-
+@bot.command()
+async def izbrisi_coin(ctx):
+    if ctx.author.guild_permissions.administrator:
+        server_id = ctx.guild.id
+        server = session.query(Server).all()
+        server = map(lambda x: x.server_id, server)
+        if int(server_id) not in list(server):
+            await ctx.send("Niste postavili kanal za dionice!")
+        else:
+            existing_server = session.query(Server).filter_by(server_id=int(server_id)).first()
+            message = ctx.message.content
+            message = message.split(" ")
+            if len(message) == 2:
+                if message[1] in existing_server.stocks.keys():
+                    users_money = session.query(Stocks).filter(Stocks.stocks.like(f"%{message[1]}%")).all()
+                    for user in users_money:
+                        user_money = session.query(Bank).filter_by(user_id=int(user.user_id)).first()
+                        user_money.money += user.stocks[message[1]] * existing_server.stocks[message[1]]
+                        del user.stocks[message[1]]
+                        flag_modified(user, "stocks")
+                        session.commit()
+                    del existing_server.stocks[message[1]]
+                    flag_modified(existing_server, "stocks")
+                    session.commit()
+                    await ctx.send(f"Uspjesno izbrisan coin {message[1]}!")
+            else:
+                await ctx.send("Pogresan format komande! Upisi !izbrisi_coin <ime_coina>")
 with open('token.txt', 'r') as f:
     token = f.read()
 bot.run(str(token))
